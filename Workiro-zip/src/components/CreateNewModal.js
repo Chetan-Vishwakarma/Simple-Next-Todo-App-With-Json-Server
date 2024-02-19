ca6ee8517268afe9bb2c8497f5737b8e854e233b
sadiquesheikh <sadique@docusoft.net>
2024-02-19 08:34:23 +0000

    header & side bar design

--- Workiro-zip/src/components/CreateNewModal.js
+++ Workiro-zip/src/components/CreateNewModal.js
@@ -5,47 +5,47 @@ import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
 };
 
 export default function CreateNewModal() {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
-  return (
-    <Box>
+    return (
+        <Box>
 
 
-      <Box className="text-center">
-      <Button className='btn-blue btn-round btn-block' onClick={handleOpen}><span class="material-symbols-outlined pe-2">
-                edit_square
-              </span> Create New</Button>
-      </Box>
+            <Box className="text-center">
+                <Button className='btn-blue btn-round btn-block' onClick={handleOpen}><span class="material-symbols-outlined pe-2">
+                    edit_square
+                </span> <span className='create-text'>Create New</span></Button>
+            </Box>
 
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        Text in a modal
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                    </Typography>
+                </Box>
+            </Modal>
         </Box>
-      </Modal>
-    </Box>
-  );
+    );
 }
