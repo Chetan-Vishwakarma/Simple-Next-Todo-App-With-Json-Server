f7d2c21e4e5a73b7827f8643410c772a03cd6324
sadiquesheikh <sadique@docusoft.net>
2024-02-29 17:02:59 +0000

    new modal design

--- Workiro-zip/src/components/TodoList.js
+++ Workiro-zip/src/components/TodoList.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
@@ -10,11 +10,35 @@ import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import CommanCLS from '../services/CommanService';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 
 function TodoList() {
+
+    // modal start
+    // modal start
+    const [openModal, setOpen] = React.useState(false);
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setOpen(false);
+    };
+    // modal end
+    // modal end
+
 
     const [agrno, setAgrNo] = useState(localStorage.getItem("agrno"));
     const [password, setPassword] = useState(localStorage.getItem("Password"));
@@ -25,7 +49,7 @@ function TodoList() {
 
     let Cls = new CommanCLS(baseUrl, agrno, Email, password);
 
-    const [allTask,setAllTask] = useState([]);
+    const [allTask, setAllTask] = useState([]);
 
     const [anchorEl, setAnchorEl] = React.useState(null);
     const open = Boolean(anchorEl);
@@ -35,13 +59,13 @@ function TodoList() {
     const handleClose = () => {
         setAnchorEl(null);
     };
-    const Json_CRM_GetOutlookTask=()=>{
+    const Json_CRM_GetOutlookTask = () => {
         let obj = {
             agrno: agrno,
             Email: Email,
             password: password
         };
-        try{
+        try {
             Cls.Json_CRM_GetOutlookTask(obj, (sts, data) => {
                 if (sts) {
                     if (data) {
@@ -51,18 +75,18 @@ function TodoList() {
                     }
                 }
             });
-        }catch(err){
-            console.log("Error while calling Json_CRM_GetOutlookTask",err);
+        } catch (err) {
+            console.log("Error while calling Json_CRM_GetOutlookTask", err);
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         setAgrNo(localStorage.getItem("agrno"));
         setFolderId(localStorage.getItem("FolderId"));
         setPassword(localStorage.getItem("Password"));
         setEmail(localStorage.getItem("Email"));
         Json_CRM_GetOutlookTask();
-    },[]);
+    }, []);
 
     function startFormattingDate(dt) {
         const timestamp = parseInt(/\d+/.exec(dt));
@@ -73,11 +97,202 @@ function TodoList() {
             year: "numeric",
         });
 
-        return formattedDate==="Invalid Date"?"01/01/1900":formattedDate;
+        return formattedDate === "Invalid Date" ? "01/01/1900" : formattedDate;
     }
+
+
+    // status dropdown
+    const [anchorElSelect, setAnchorElSelect] = React.useState(null);
+    const openDropdown = Boolean(anchorElSelect);
+    const handleClickDroppdown = (event) => {
+        setAnchorElSelect(event.currentTarget);
+    };
+    const handleCloseDropdown = () => {
+        setAnchorElSelect(null);
+    };
 
     return (
         <Box className="container-fluid p-0">
+
+
+            {/* modal start */}
+            <React.Fragment>
+
+
+
+                <Dialog
+                    fullScreen={fullScreen}
+                    open={openModal}
+                    onClose={handleCloseModal}
+                    aria-labelledby="responsive-dialog-title"
+                    className='custom-modal'
+                >
+
+                    <DialogContent>
+                        <DialogContentText>
+
+                            <Box className="d-flex align-items-center justify-content-between">
+                                <Box className="d-flex">
+
+                                    <div>
+                                        <Button
+                                            id="fade-button5"
+                                            aria-controls={openDropdown ? 'fade-menu' : undefined}
+                                            aria-haspopup="true"
+                                            aria-expanded={openDropdown ? 'true' : undefined}
+                                            onClick={handleClickDroppdown}
+                                        >
+
+                                            <CheckCircleIcon />
+
+                                        </Button>
+                                        <Menu
+                                            id="fade-menu"
+                                            MenuListProps={{
+                                                'aria-labelledby': 'fade-button5',
+                                            }}
+                                            anchorElSelect={anchorElSelect}
+                                            open={openDropdown}
+                                            onClose={handleCloseDropdown}
+                                        >
+                                            <MenuItem onClick={handleCloseDropdown}>Profile</MenuItem>
+                                            <MenuItem onClick={handleCloseDropdown}>My account</MenuItem>
+                                            <MenuItem onClick={handleCloseDropdown}>Logout</MenuItem>
+                                        </Menu>
+                                    </div>
+
+
+                                    {/* <div>
+                                        <Button
+                                            id="basic-button-status"
+                                            aria-controls={open ? 'basic-menu' : undefined}
+                                            aria-haspopup="true"
+                                            aria-expanded={open ? 'true' : undefined}
+                                            onClick={handleClick}
+                                        >
+                                            <CheckCircleIcon />
+                                            
+                                        </Button>
+                                        <Menu
+                                            id="basic-menu"
+                                            anchorEl={anchorEl}
+                                            open={open}
+                                            onClose={handleClose}
+                                            MenuListProps={{
+                                                'aria-labelledby': 'basic-button-status',
+                                            }}
+                                        >
+                                            <MenuItem onClick={handleClose}>Profile</MenuItem>
+                                            <MenuItem onClick={handleClose}>My account</MenuItem>
+                                            <MenuItem onClick={handleClose}>Logout</MenuItem>
+                                        </Menu>
+                                    </div> */}
+
+                                    <Typography variant='subtitle1' className='font-16 sembold mb-2'>Select Type</Typography>
+
+
+                                </Box>
+
+                                <Box className="d-flex">
+
+                                    <div>
+                                        <Button
+                                            id="fade-button"
+                                            aria-controls={open ? 'fade-menu' : undefined}
+                                            aria-haspopup="true"
+                                            aria-expanded={open ? 'true' : undefined}
+                                            onClick={handleClick}
+                                            className='min-width-auto'
+                                        >
+                                            Priority
+                                        </Button>
+                                        <Menu
+                                            id="fade-menu"
+                                            MenuListProps={{
+                                                'aria-labelledby': 'fade-button',
+                                            }}
+                                            anchorEl={anchorEl}
+                                            open={open}
+                                            onClose={handleClose}
+                                        >
+                                            <MenuItem onClick={handleClose}>Profile</MenuItem>
+                                            <MenuItem onClick={handleClose}>My account</MenuItem>
+                                        </Menu>
+                                    </div>
+
+                                    <div>
+                                        <Button
+                                            id="fade-button"
+                                            aria-controls={open ? 'fade-menu' : undefined}
+                                            aria-haspopup="true"
+                                            aria-expanded={open ? 'true' : undefined}
+                                            onClick={handleClick}
+                                            className='min-width-auto px-0 text-gray'
+                                        >
+
+                                            <MoreVertIcon />
+
+                                        </Button>
+                                        <Menu
+                                            id="fade-menu"
+                                            MenuListProps={{
+                                                'aria-labelledby': 'fade-button',
+                                            }}
+                                            anchorEl={anchorEl}
+                                            open={open}
+                                            onClose={handleClose}
+                                        >
+                                            <MenuItem onClick={handleClose}>Profile</MenuItem>
+                                            <MenuItem onClick={handleClose}>My account</MenuItem>
+                                        </Menu>
+                                    </div>
+
+                                    <Button onClick={handleClose} autoFocus sx={{ minWidth: 30, }}>
+                                        <span className="material-symbols-outlined text-black">
+                                            cancel
+                                        </span>
+                                    </Button>
+
+                                </Box>
+
+
+
+
+                            </Box>
+
+                            <hr />
+
+                            <Typography variant="h4">Contact Agreement Update</Typography>
+
+                            <Box className='d-flex'>
+                                <Box className='d-flex'>
+                                    <p>Client: ABC Limited</p>
+                                </Box>
+                            </Box>
+
+
+                        </DialogContentText>
+
+                        {/* <hr /> */}
+
+                        <DialogActions className='px-0 w-100'>
+                            <Box className="d-flex align-items-center justify-content-end w-100">
+
+                                {/* <Box>
+                <Button autoFocus className='btn-red me-2' onClick={handleCloseModal}>
+                    cancel
+                </Button>
+                <Button className='btn-green' onClick={handleCloseModal} autoFocus>
+                    Save
+                </Button>
+            </Box> */}
+                            </Box>
+                        </DialogActions>
+                    </DialogContent>
+                </Dialog>
+            </React.Fragment>
+            {/* modal end */}
+
 
             <Typography variant='subtitle1' className='font-18 bold mb-2'>Select Filter</Typography>
             <Box className="d-flex align-items-center mb-4 flex-wrap">
@@ -100,21 +315,21 @@ function TodoList() {
 
             <Box className='row'>
                 {
-                    allTask.length>0 &&
-                        allTask.map((item,index)=>{
-                            return <Box key={index} className='col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-sm-6 d-flex'>
-                            <Box className='todo-list-box white-box relative w-100'>
-        
+                    allTask.length > 0 &&
+                    allTask.map((item, index) => {
+                        return <Box key={index} className='col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-sm-6 d-flex'>
+                            <Box className='todo-list-box white-box relative w-100' onClick={handleClickOpen}>
+
                                 <Checkbox className='text-blue check-todo'
                                     {...label}
                                     icon={<RadioButtonUncheckedIcon />}
                                     checkedIcon={<CheckCircleIcon />}
                                 />
-        
+
                                 <Typography variant='subtitle1 mb-4 d-block'><strong>Type:</strong> {item.Source}</Typography>
-        
+
                                 <Typography variant='h2' className='mb-2'>{item.Subject}</Typography>
-        
+
                                 <Box className='d-flex align-items-center justify-content-between'>
                                     <Typography variant='subtitle1'><pan className='text-gray'>
                                         {item.UserName} <ArrowForwardIosIcon className='font-14' /> </pan>
@@ -122,11 +337,11 @@ function TodoList() {
                                         <a href='#'>{item["Forwarded By"]}</a> <a href='#'> +1</a></Typography>
                                     <Typography variant='subtitle1 sembold'>{startFormattingDate(item["EndDateTime"])}</Typography>
                                 </Box>
-        
+
                                 <Box className='d-flex align-items-center justify-content-between'>
                                     <Typography variant='subtitle1'>{item.Client}</Typography>
                                     <Typography variant='subtitle1'>
-        
+
                                         <Box>
                                             <Button
                                                 id="basic-button"
@@ -152,18 +367,18 @@ function TodoList() {
                                                 <MenuItem onClick={handleClose}>Low</MenuItem>
                                             </Menu>
                                         </Box>
-        
+
                                     </Typography>
                                 </Box>
-        
+
                                 <Box className='mt-2'>
                                     <Button variant="text" className='btn-blue-2 me-2'>Action</Button>
                                     <Button variant="text" className='btn-blue-2'>Defer</Button>
                                 </Box>
-        
+
                             </Box>
                         </Box>
-                        })
+                    })
                 }
 
                 {/* <Box className='col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-sm-6 d-flex'>
