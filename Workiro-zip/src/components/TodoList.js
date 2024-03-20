55dc247cba375e824a8db204e688fdaf7b53d301
sunil <sunilkewat@docusoft.net>
2024-03-20 11:03:17 +0000

    Signing

--- Workiro-zip/src/components/TodoList.js
+++ Workiro-zip/src/components/TodoList.js
@@ -56,7 +56,7 @@ function TodoList() {
                     if (data) {
                         let json = JSON.parse(data);
                         console.log("Json_CRM_GetOutlookTask", json);
-                        let result = json.Table.filter((el)=>el.Source==="CRM");
+                        let result = json.Table.filter((el)=>el.Source==="CRM" || el.Source==="Portal");
                         const formattedTasks = result.map((task) => {
                             let timestamp;
                             if (task.EndDateTime) {
