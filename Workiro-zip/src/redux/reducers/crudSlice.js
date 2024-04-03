2fdce9eb3720a79f85a260faecbe4ad1583c9da2
Chetan-Vishwakarma <vishwakarmachetan569@gmail.com>
2024-04-03 14:18:02 +0100

    Applied redux functionality on MyTask(TodoList.js) set api data into redux

--- Workiro-zip/src/redux/reducers/crudSlice.js
+++ Workiro-zip/src/redux/reducers/crudSlice.js
@@ -1,46 +1,47 @@
-import {createSlice} from "@reduxjs/toolkit";
-import axios from "axios";
+// UNCOMMENT TO READ THIS CODE THIS IS AN EXAMPLE OF HOW TO MANAGE API'S IN REDUX
+// import {createSlice} from "@reduxjs/toolkit";
+// import axios from "axios";
 
-const crudSlice = createSlice({
-    name:"crud",
-    initialState: {
-        value:0,
-    },
-    reducers: {
-        getAllUsers: (state,action)=>{
-            state.value = action.payload;
-        },
-        deleteUser: (state, action)=>{
-            // console.log("sdfdsgsdg",action.payload.id);
-            // console.log("sdfdsgsdg",action.payload.obj);
-            let target = action.payload.id;
-            let data = action.payload.obj;
-            let filteredData = data.filter((item)=>{
-                return item.id != target;
-            });
-            state.value = filteredData;
-        },
-        updateUser: (state, action)=>{
-            let id = action.payload.id;
-            let username = action.payload.username;
-            let email = action.payload.email;
-            let data = action.payload.obj;
-            data.map((item)=>{
-                if(item.id==id){
-                    item.username = username;
-                    item.email = email;
-                }
-            });
-            state.value = data;
-        }
-    }
-});
+// const crudSlice = createSlice({
+//     name:"crud",
+//     initialState: {
+//         value:0,
+//     },
+//     reducers: {
+//         getAllUsers: (state,action)=>{
+//             state.value = action.payload;
+//         },
+//         deleteUser: (state, action)=>{
+//             // console.log("sdfdsgsdg",action.payload.id);
+//             // console.log("sdfdsgsdg",action.payload.obj);
+//             let target = action.payload.id;
+//             let data = action.payload.obj;
+//             let filteredData = data.filter((item)=>{
+//                 return item.id != target;
+//             });
+//             state.value = filteredData;
+//         },
+//         updateUser: (state, action)=>{
+//             let id = action.payload.id;
+//             let username = action.payload.username;
+//             let email = action.payload.email;
+//             let data = action.payload.obj;
+//             data.map((item)=>{
+//                 if(item.id==id){
+//                     item.username = username;
+//                     item.email = email;
+//                 }
+//             });
+//             state.value = data;
+//         }
+//     }
+// });
 
-export const {getAllUsers,deleteUser,updateUser} = crudSlice.actions;
+// export const {getAllUsers,deleteUser,updateUser} = crudSlice.actions;
 
-export const getUsers = () => async (dispatch) => {
-    const response = await axios.get("https://jsonplaceholder.typicode.com/users");
-    dispatch(getAllUsers(response.data))
-}
+// export const getUsers = () => async (dispatch) => {
+//     const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+//     dispatch(getAllUsers(response.data))
+// }
 
-export default crudSlice.reducer;
+// export default crudSlice.reducer;
