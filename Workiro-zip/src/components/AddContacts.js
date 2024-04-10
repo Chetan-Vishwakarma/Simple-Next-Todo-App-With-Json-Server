79558526e2697a68203b55cf0d1d7e91f2fbf8f1
sadiquesheikh <sadique@docusoft.net>
2024-04-10 14:54:58 +0530

    design changes

--- Workiro-zip/src/components/AddContacts.js
+++ Workiro-zip/src/components/AddContacts.js
@@ -572,7 +572,7 @@ function AddContacts() {
     const clientName = localStorage.getItem("ClientName");
 
     // Update userContactDetails state with the retrieved value
-    if(clientName){
+    if (clientName) {
       setContactDetails(prevState => ({
         ...prevState,
         ReferenceID: clientName
@@ -587,18 +587,229 @@ function AddContacts() {
 
 
       <Box sx={{ width: '100%', typography: 'body1' }} className="mt-3">
-        <TabContext value={value}>
+
+
+        <Box className="general-tab white-box">
+          <Box className='d-flex'>
+            <Box className="mb-3 pe-4 border-end me-4">
+              <UploadButtons
+                userContactDetails={userContactDetails}
+                setContactDetails={setContactDetails}
+              />
+            </Box>
+
+            <Box className="mb-3 w-100">
+
+              <Box className='well mb-4'>
+
+                <h2 className='font-20 mb-3 text-black'>Contact Details</h2>
+                <Box className='well mb-4'>
+                  <h2 className='font-14 bold mb-2 text-black'>Import from Companies House</h2>
+                  <Grid container spacing={2}>
+                    <Grid item xs={6} md={6}>
+                      <Autocomplete
+                        {...contactlist}
+                        id="contactlist"
+                        clearOnEscape
+                        onChange={onChangecontactlist}
+                        renderInput={(params) => (
+                          <TextField
+                            {...params}
+                            variant="outlined"
+                            name="contactlist"
+                            value={""}
+                            //   onChange={onChange}
+                            label="Contact List"
+                          />
+                        )}
+                      />
+                    </Grid>
+
+                    <Grid item xs={6} md={6}>
+                      {/* <TextField
+                // {...params}wid
+                fullWidth
+                variant="outlined"
+                name="importclient"
+                // value={Importdata}
+                onKeyDown={handleKeyDown}
+                onChange={onChangeImportData}
+                label="Import List"
+              /> */}
+                      <Autocomplete
+                        fullWidth
+                        // options={ImportContact.map((option) => option.title)}
+                        options={ImportContact} // Pass the entire ImportContact array
+                        getOptionLabel={(option) => option.FirstName + " " + option.LastName}
+                        onChange={(e, value) => setImportdata(value)}
+                        onKeyDown={handleKeyDown}
+                        // inputValue={ImportContact}
+                        noOptionsText="No matches found"
+                        filterOptions={(x) => x}
+                        autoComplete
+                        includeInputInList
+                        value={txtValue}
+                        open={open} // Controlled by state
+                        onOpen={() => setOpen(true)} // Open the Autocomplete dropdown
+                        onClose={() => setOpen(false)} // Close the Autocomplete dropdown
+                        renderOption={(props, option) => {
+                          // Custom rendering for each option
+                          console.log(option, "rendwered dynamic from apifff", props);
+                          return (
+                            !option.resigned_on && (
+                              <div>
+                                <li
+                                  {...props}
+                                  onClick={() => {
+                                    handleOptionClick(option); // Pass the id directly
+                                  }}
+                                >
+                                  <Grid container alignItems="center">
+                                    <Grid item sx={{ width: 'calc(100% - 44px)', wordWrap: 'break-word' }}>
+                                      {option.FirstName + " " + option.LastName}
+                                    </Grid>
+                                  </Grid>
+                                </li>
+                              </div>
+                            )
+
+                          );
+                        }}
+                        renderInput={(params) => (
+                          <TextField
+                            {...params}
+                            fullWidth
+                            variant="outlined"
+                            name="importclient"
+                            onChange={onChangeImportData}
+                            label="Enter Company Name"
+                          />
+                        )}
+                      />
+                    </Grid>
+                  </Grid>
+                </Box>
+
+                <Grid container spacing={2}>
+                  <Grid item xs={6} md={6}>
+                    <Autocomplete
+                      {...clientlist}
+                      id="clientlist"
+                      clearOnEscape
+                      onChange={onChangeclientlist}
+                      renderInput={(params) => (
+                        <TextField
+                          {...params}
+                          variant="outlined"
+                          name="Selectclient"
+                          value={""}
+                          //   onChange={onChange}
+                          label="Folder List"
+                        />
+                      )}
+                    />
+                  </Grid>
+
+                  <Grid item xs={6} md={6}>
+                    <Autocomplete
+                      {...bussinesslist}
+                      id="clear-on-escape-teams"
+                      clearOnEscape
+                      onChange={onChangebussines}
+                      renderInput={(params) => (
+                        <TextField
+                          {...params}
+                          name="Selectteamsa"
+                          value={""}
+                          //   onChange={onChangebussines}
+                          label="Reference List"
+                          variant="outlined"
+                        />
+                      )}
+                    />
+                  </Grid>
+
+                  <Grid item xs={12} md={12}>
+                    <ContactMainform
+                      clientNames={clientNames}
+                      contact={contact}
+                      setContact={setContact}
+                      contactlistdata={contactlistdata}
+                      userContactDetails={userContactDetails}
+                      setContactDetails={setContactDetails}
+                      Importcontactdata={Importcontactdata}
+                      setImportcontactdata={setImportcontactdata}
+                    />
+                  </Grid>
+
+                </Grid>
+              </Box>
+
+              {/* <hr /> */}
+
+              <Box className='well mb-4'>
+                <h2 className='font-20 mb-3 text-black'>AML Details</h2>
+              </Box>
+
+              {/* <hr /> */}
+
+              <Box className='well mb-4'>
+                <h2 className='font-20 mb-3 text-black'>UDF Details</h2>
+                <ContactUDF
+                  data={clientDetails}
+                  setDataFromChild={setDataFromChild}
+                ></ContactUDF>
+              </Box>
+
+              <Button
+                  style={{ marginTop: "5px" }}
+                  variant="contained"
+                  disabled={!clientData || !selectedFolderID}
+                  onClick={handleSubmit}
+                  className='btn-blue-2'
+                >
+                  Add New Contact
+                </Button>
+
+
+            </Box>
+          </Box>
+
+          <Box className="mb-3">
+            {/* <MainContact 
+      userContactDetails={userContactDetails}
+      setContactDetails={setContactDetails}
+      /> */}
+            <Grid container spacing={3}>
+
+            </Grid>
+          </Box>
+
+          {/* <Box className='text-end'>
+      <Button
+        style={{ marginTop: "5px" }}
+        variant="contained"
+        disabled={!clientData || !selectedFolderID}
+        onClick={handleSubmit}
+        className='btn-blue-2'
+      >
+        Add New Contact
+      </Button>
+    </Box> */}
+        </Box>
+
+        {/* end */}
+
+
+        {/* <TabContext value={value}>
           <Box className='mb-1'>
             <TabList onChange={handleChange} aria-label="lab API tabs example" className='custom-tabs'>
               <Tab label="Contact Details" value="1" />
               <Tab label="AML Details" value="2" />
               <Tab label="UDF Details" value="3" />
-              {/* <Tab label="Send Proposal" value="4" />
-                            <Tab label="Portal User" value="5" />
-                            <Tab label="Companies House" value="6" />
-                            <Tab label="Requested Document" value="7" /> */}
             </TabList>
           </Box>
+
           <TabPanel value="1" className='p-0'>
             <Box className="general-tab white-box">
               <Box className='d-flex'>
@@ -626,7 +837,6 @@ function AddContacts() {
                               variant="outlined"
                               name="contactlist"
                               value={""}
-                              //   onChange={onChange}
                               label="Contact List"
                             />
                           )}
@@ -634,16 +844,7 @@ function AddContacts() {
                       </Grid>
 
                       <Grid item xs={6} md={6}>
-                        {/* <TextField
-                        // {...params}wid
-                        fullWidth
-                        variant="outlined"
-                        name="importclient"
-                        // value={Importdata}
-                        onKeyDown={handleKeyDown}
-                        onChange={onChangeImportData}
-                        label="Import List"
-                      /> */}
+
                         <Autocomplete
                           fullWidth
                           // options={ImportContact.map((option) => option.title)}
@@ -752,14 +953,12 @@ function AddContacts() {
 
                   </Grid>
 
+
                 </Box>
               </Box>
 
               <Box className="mb-3">
-                {/* <MainContact 
-              userContactDetails={userContactDetails}
-              setContactDetails={setContactDetails}
-              /> */}
+                
                 <Grid container spacing={3}>
 
                 </Grid>
@@ -781,10 +980,9 @@ function AddContacts() {
           </TabPanel>
 
           <TabPanel value="2" className='p-0'>
-            {/* <ClientAddress></ClientAddress> */}
           </TabPanel>
+
           <TabPanel value="3">
-            {/* <Contact></Contact> */}
             <Box className="general-tab white-box">
               <ContactUDF
                 data={clientDetails}
@@ -793,22 +991,10 @@ function AddContacts() {
             </Box>
 
           </TabPanel>
-          <TabPanel value="4">
-            <TaskList></TaskList>
-          </TabPanel>
 
-          {/* <TabPanel value="5" className='p-0'>
-                        <DocumentList clientId={originatorNo} globalSearchDocs={globalSearchDocs} ></DocumentList>
-                    </TabPanel> */}
+        </TabContext> */}
 
-          {/* <TabPanel value="5">
-                        <DocumentList/>
-                    </TabPanel> */}
-          <TabPanel value="6" className='p-0'>
-            <CompaniesHouse></CompaniesHouse>
-          </TabPanel>
-          <TabPanel value="7">Item Three</TabPanel>
-        </TabContext>
+
       </Box>
     </Box>
   )
