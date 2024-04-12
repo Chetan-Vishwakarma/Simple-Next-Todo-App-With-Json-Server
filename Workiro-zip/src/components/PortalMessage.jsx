d1054cd11d13dc557dbbe889031f7e32225d8e48
sadiquesheikh <sadique@docusoft.net>
2024-04-12 15:10:18 +0530

    design changes

--- Workiro-zip/src/components/PortalMessage.jsx
+++ Workiro-zip/src/components/PortalMessage.jsx
@@ -846,8 +846,8 @@ const PortalMessage = ({ selectedTask, Json_RegisterItem, setPortalComments, set
                                     </>) : (<>
                                         <Box className='d-flex align-items-center'>
                                             {/* {<CopyLinkButton copyLink={copyLink}></CopyLinkButton>} */}
-                                            <HourglassEmptyIcon className='text-warning' />
-                                            <h5 className='font-14 text-black mb-1'>Pending Approval</h5>
+                                            <HourglassEmptyIcon className='text-warning me-1' />
+                                            <h5 className='font-14 text-black mb-0'>Pending Approval</h5>
                                             {/* <Button className='btn-blue-2' size="small" onClick={HandalChangeSendReminder} startIcon={<ScheduleIcon />}>Send Reminder</Button> */}
                                         </Box>
 
@@ -1113,7 +1113,7 @@ const PortalMessage = ({ selectedTask, Json_RegisterItem, setPortalComments, set
                                                                         <HourglassEmptyIcon className='text-gray' />
                                                                         <Box className='ps-2'>
                                                                             {/* {<CopyLinkButton copyLink={copyLink}></CopyLinkButton>} */}
-                                                                            <h5 className='font-14 text-black mb-1'>Document Disapproved</h5>
+                                                                            <h5 className='font-14 text-black'>Document Disapproved</h5>
                                                                             {/* <Button className='btn-blue-2' size="small" onClick={HandalChangeSendReminder} startIcon={<ScheduleIcon />}>Send Reminder</Button> */}
                                                                         </Box>
                                                                     </Box>
