a6a12d7ad9911df1c95fc7be2232dfb2eb080c32
sonam choudhari <sonam.cho346@gmail.com>
2024-04-24 15:41:24 +0100

    export to excel

--- Workiro-zip/src/client/utils/Activitygrid.js
+++ Workiro-zip/src/client/utils/Activitygrid.js
@@ -8,7 +8,7 @@ import DataGrid, {
 import { Box, Typography, Button, Paper, Grid, TextField, Autocomplete } from '@mui/material';
 import 'devextreme/dist/css/dx.light.css';
 import CommanCLS from '../../services/CommanService';
-export default function Activitygrid({selectedDocument,call_Json_GetAudit,getAudit,tempdatafilter}) {
+export default function Activitygrid({grid,selectedDocument,call_Json_GetAudit,getAudit,tempdatafilter}) {
   console.log('tempselectedDocumentsonam',tempdatafilter,"call_Json_GetAuditsonam",call_Json_GetAudit,"getAuditsonam",getAudit);
   const [dataNotFoundBoolean, setDataNotFoundBoolean] = useState(false);
   const [advFilteredResult, setAdvFilteredResult] = useState([]);
@@ -28,6 +28,7 @@ export default function Activitygrid({selectedDocument,call_Json_GetAudit,getAud
   return (
     <div><Box className=''>
     <DataGrid
+        ref={grid}
         id="dataGrid"
         className='table-grid'
         // style={{ width: "100%" }}
@@ -38,12 +39,6 @@ export default function Activitygrid({selectedDocument,call_Json_GetAudit,getAud
         <Column dataField="Actioned Date" dataType="date" caption="Date"  format="M/d/yyyy, HH:mm" />
         <Column dataField="Comments" dataType="string" caption="Activity" />
         <Column dataField="ForwardedBy" dataType="string" caption="User" />
-        {/* <Column dataField="Received Date" dataType="date" caption="Received Date" />
-        <Column dataField="Category" dataType="string" caption="Category" />
-        <Column dataField="Client" dataType="string" caption="Reference" />
-        <Column dataField="FileSize" dataType="string" caption="File Size" /> */}
-        {/* <FilterRow visible={true} /> */}
-        {/* <FilterPanel visible={true} /> */}
         <HeaderFilter visible={true} />
         <Scrolling mode="standard" />
         <Selection
