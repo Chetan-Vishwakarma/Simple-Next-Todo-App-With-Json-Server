c6e5c8fc391b0bcb475b8517574fe529e1698515
Chetan-Vishwakarma <vishwakarmachetan569@gmail.com>
2024-05-07 13:34:22 +0100

    After refactoring the code of Mytask's creationdate and enddatetime

--- Workiro-zip/src/redux/reducers/counterSlice.js
+++ Workiro-zip/src/redux/reducers/counterSlice.js
@@ -137,64 +137,14 @@ export const fetchAllTasksRedux = (target) => dispatch => {
               return;
             } else if (target === "MyTask") {
               const formattedTasks = fltTask.map((task) => {
-                let timestamp;
-                let timestamp2;
-                let date = "";
-                let date2 = "";
-                if (task.CreationDate) {
-                  timestamp = parseInt(task.CreationDate.slice(6, -2));
-                  if (!isNaN(timestamp)) {
-                    date = new Date(timestamp); // Create Date object using timestamp
-                  } else {
-                    // console.error("Invalid timestamp:", timestamp);
-                  }
-                } else {
-                  date = task.CreationDate;
-                }
-                if (task.EndDateTime) {
-                  timestamp2 = parseInt(task.EndDateTime.slice(6, -2));
-                  if (!isNaN(timestamp2)) {
-                    date2 = new Date(timestamp2); // Create Date object using timestamp2
-                  } else {
-                    // console.error("Invalid timestamp2:", timestamp2);
-                  }
-                } else {
-                  date2 = task.EndDateTime;
-                }
-
-                return { ...task, CreationDate: date, EndDateTime: date2 };
+                return dateForMyTask(task);
               });
               formattedTasks.sort((a, b) => b.EndDateTime - a.EndDateTime);
               dispatch(fetchAllTasks(formattedTasks));
               return;
             } else {
               const formattedTasks = fltTask.map((task) => {
-                let timestamp;
-                let timestamp2;
-                let date = "";
-                let date2 = "";
-                if (task.CreationDate) {
-                  timestamp = parseInt(task.CreationDate.slice(6, -2));
-                  if (!isNaN(timestamp)) {
-                    date = new Date(timestamp); // Create Date object using timestamp
-                  } else {
-                    // console.error("Invalid timestamp:", timestamp);
-                  }
-                } else {
-                  date = task.CreationDate;
-                }
-                if (task.EndDateTime) {
-                  timestamp2 = parseInt(task.EndDateTime.slice(6, -2));
-                  if (!isNaN(timestamp2)) {
-                    date2 = new Date(timestamp2); // Create Date object using timestamp2
-                  } else {
-                    // console.error("Invalid timestamp2:", timestamp2);
-                  }
-                } else {
-                  date2 = task.EndDateTime;
-                }
-
-                return { ...task, CreationDate: date, EndDateTime: date2 };
+                return dateForMyTask(task);
               });
               formattedTasks.sort((a, b) => b.EndDateTime - a.EndDateTime);
               dispatch(fetchAllTasks(formattedTasks));
