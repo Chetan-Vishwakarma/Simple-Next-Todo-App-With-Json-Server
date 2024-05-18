2b9b4e732f6372715f8d191e0a27cba73d55443c
Chetan-Vishwakarma <vishwakarmachetan569@gmail.com>
2024-05-18 11:01:02 +0100

    Removed unneccesary code from Activitygrid.js component

--- Workiro-zip/src/client/utils/Activitygrid.js
+++ Workiro-zip/src/client/utils/Activitygrid.js
@@ -1,30 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import DataGrid, {
-  Column, FilterRow, Search, SearchPanel, Selection,
+  Column, SearchPanel, Selection,
   HeaderFilter, Scrolling,
-  FilterPanel,
-  Pager, Paging, DataGridTypes, FormGroup,
+  Pager, Paging,
 } from 'devextreme-react/data-grid';
-import { Box, Typography, Button, Paper, Grid, TextField, Autocomplete } from '@mui/material';
+import { Box } from '@mui/material';
 import 'devextreme/dist/css/dx.light.css';
-import CommanCLS from '../../services/CommanService';
 export default function Activitygrid({grid,selectedDocument,call_Json_GetAudit,getAudit,tempdatafilter}) {
-  console.log('tempselectedDocumentsonam',tempdatafilter,"call_Json_GetAuditsonam",call_Json_GetAudit,"getAuditsonam",getAudit);
-  const [dataNotFoundBoolean, setDataNotFoundBoolean] = useState(false);
-  const [advFilteredResult, setAdvFilteredResult] = useState([]);
-  const [documents, setDocuments] = useState([]);
-  const [agrno, setAgrNo] = useState(localStorage.getItem("agrno"));
-  const [password, setPassword] = useState(localStorage.getItem("Password"));
-  const [Email, setEmail] = useState(localStorage.getItem("Email"));
-  const [folderId, setFolderId] = useState(localStorage.getItem("FolderId"));
-  const baseUrl = "https://docusms.uk/dsdesktopwebservice.asmx/";// base url for api
 
-  //   let dt = new LoginDetails();
-  let cls = new CommanCLS(baseUrl, agrno, Email, password);
-  useEffect(() => {
-  
-   
-  }, [])
   return (
     <div><Box className=''>
     <DataGrid
